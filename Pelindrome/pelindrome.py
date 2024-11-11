152e85bc0d714dea4c57420afd10c6c27ac6f3b9
Benson oyoo <114955918+Oyoo22@users.noreply.github.com>
2024-11-11 19:08:19 +0300

    Add files via upload

---
+++ Pelindrome/pelindrome.py
@@ -0,0 +1,8 @@
+# Test the function.
+print(is_palindrome("Racecar"))  # Output: True
+print(is_palindrome("Hello"))    # Output: False
+
+# Test even numbers filter function.
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = filter_even_numbers(numbers)
+print(even_numbers)  # Output: [2, 4, 6]
